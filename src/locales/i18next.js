8888cb3cb42cf1ded79b5c1bde028a20c70c0ddb
omarkhodjaev <umarkhankhodjaev.com>
2022-12-08 04:56:15 +0500

    Header off

--- src/locales/i18next.js
+++ src/locales/i18next.js
@@ -1,30 +1,30 @@
-// import i18n from "i18next";
-// import Backend from "i18next-http-backend";
-// import LanguageDetector from "i18next-browser-languagedetector";
-// import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
-// i18n.use(Backend)
-// 	.use(LanguageDetector)
-// 	.use(initReactI18next)
-// 	.init({
-// 		backend: {
-// 			// translation file path
-// 			loadPath: "/assets/i18n/{{ns}}/{{lng}}.json",
-// 		},
-// 		fallbackLng: "uz",
-// 		// disabled in production
-// 		debug: false,
-// 		// can have multiple namespaces, in case you want to divide a huge
-// 		// translation into smaller pieces and load them on demand
-// 		ns: ["home", "profile"],
+i18n.use(Backend)
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		backend: {
+			// translation file path
+			loadPath: "/assets/i18n/{{ns}}/{{lng}}.json",
+		},
+		fallbackLng: "uz",
+		// disabled in production
+		debug: false,
+		// can have multiple namespaces, in case you want to divide a huge
+		// translation into smaller pieces and load them on demand
+		ns: ["home", "profile"],
 
-// 		interpolation: {
-// 			espaceValue: false,
-// 			formatSeparator: ",",
-// 		},
-// 		react: {
-// 			wait: true,
-// 		},
-// 	});
+		interpolation: {
+			espaceValue: false,
+			formatSeparator: ",",
+		},
+		react: {
+			wait: true,
+		},
+	});
 
-// export default i18n;
+export default i18n;
